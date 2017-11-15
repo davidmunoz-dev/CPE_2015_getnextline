41d276b5cdfeda70306b4e3fb5f42ae4427319ac
David Munoz <david.munozfr@gmail.com>
2017-11-15 15:56:59 +0100

    getnextline

---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+/*
+** get_next_line.h for hi in /rendu_ProgElem/CPE_2015_getnextline
+**
+** Made by Munoz David
+** Login   <munoz_d@epitech.net>
+**
+** Started on  Mon Jan 11 23:09:01 2016 Munoz David
+** Last update Thu Mar 10 13:17:43 2016 David Munoz
+*/
+
+#ifndef READ_SIZE
+
+# define READ_SIZE (1)
+
+#endif /* !READ_SIZE */
